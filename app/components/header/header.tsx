08cdd77b03968e730df8a7c9dce202a6d88e99df
Lazea Cristian <cristian.lazea01@e-uvt.ro>
2023-12-19 22:48:29 +0200

    header for mobile bone

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -35,7 +35,7 @@ const Header = () => {
                 </Link>
             </div>
 
-            <Popover.Overlay className="fixed inset-0 bg-black opacity-30" />
+            <Popover.Overlay className="fixed inset-0 bg-white opacity-100" />
             <Transition
                 as={Fragment}
                 enter="duration-200 ease-out"
@@ -44,37 +44,55 @@ const Header = () => {
                 leave="duration-100 ease-in"
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95">
-                <Popover.Panel focus className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
-                    <div className="px-6 pt-5 pb-6 pr-4">
+                <Popover.Panel focus className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden bg-white">
+                    <div className="px-6 pt-5 pb-6">
                         <div className="flex items-center justify-between">
-                            <h1 className="font-bold">SmileSeek</h1>
-                            <div className="-mr-2">
-                                <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 
-                text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset
-                focus:ring-indigo-500">
-                                    <span className="sr-only">Close menu</span>
-                                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                                </Popover.Button>
-                            </div>
+                            <h1 className="font-bold" style={{ fontFamily: 'Newsreader, serif', color: 'black', fontSize: '20px' }}>SmileSeek</h1>
+                            <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                            </Popover.Button>
                         </div>
-                        <div className="mt-6 pr-4">
+                        <div className="mt-6">
                             <nav className="grid gap-y-8">
-                                <Link href="/" className="focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500 px-2">
-                                    Exploreaza cabinetele
-                                </Link>
-                                <Link href="/" className="focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500 px-2">
-                                    Alaturati-va ca profesionist
-                                </Link>
-                                <Link href="/" className="focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500 px-2">
-                                    Home
-                                </Link>
+                                <Button
+                                    variant="contained"
+                                    sx={{
+                                        boxShadow: 'none',
+                                        '&:hover': { boxShadow: 'none' },
+                                        color: 'black',
+                                        justifyContent: 'flex-start' // Aliniază conținutul butonului la dreapta
+                                    }}
+                                >
+                                    <Link href="/home" style={{ color: 'black', textDecoration: 'none', textAlign: 'right' }}>
+                                        Exploreaza cabinetele
+                                    </Link>
+                                </Button>
+                                <Button
+                                    variant="contained"
+                                    sx={{
+                                        boxShadow: 'none',
+                                        '&:hover': { boxShadow: 'none' },
+                                        color: 'black',
+                                        justifyContent: 'flex-start' // Aliniază conținutul butonului la dreapta
+                                    }}
+                                >
+                                    <Link href="/home" style={{ color: 'black', textDecoration: 'none', textAlign: 'right' }}>
+                                        Alaturati-va ca profesionist
+                                    </Link>
+                                </Button>
+                                <Button
+                                    variant="contained"
+                                    sx={{
+                                        boxShadow: 'none',
+                                        '&:hover': { boxShadow: 'none' },
+                                        color: 'black',
+                                        justifyContent: 'flex-start' // Aliniază conținutul butonului la dreapta
+                                    }}
+                                >
+                                    <AccountCircleIcon style={{ fontSize: 40 }} />
+                                    <span style={{ marginLeft: '8px', color: 'black' }}>Cont</span>
+                                </Button>
                             </nav>
-                        </div>
-                        <div className="mt-6 flex flex-col items-end gap-2">
-                            <Link href="/register" className="rounded-md bg-white px-4 py-2 text-sm font-medium text-black md:text-x1 w-full border-2
-              focus:outline-none focus:ring-inset focus:ring-gray-500">
-                                Cont
-                            </Link>
                         </div>
                     </div>
                 </Popover.Panel>
