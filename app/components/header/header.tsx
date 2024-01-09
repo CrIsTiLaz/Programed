bd3f8abe645efc76b91ee0acb857ff9ef6bff52c
Lazea Cristian <cristian.lazea01@e-uvt.ro>
2024-01-09 17:35:11 +0200

    remaked header

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -5,19 +5,43 @@ import { Button } from "@mui/material";
 import Link from "next/link";
 import { Fragment } from 'react';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Image from 'next/image';
+import { BubblyLink } from "../bubbly/BubblyLink";
 
+const MyBubblyLink = ({ to = "", text = "", imageSrc = "" }) => (
+    <BubblyLink
+        to={to}
+        colorStart="#175DDC"
+        colorEnd="#2B2B2B"
+        duration={1300}
+    >
+        {/* {imageSrc && <img src={imageSrc} alt="Icon" id="logo" />} */}
+        {text}
+    </BubblyLink>
+);
 const Header = () => {
     return (
-        <Popover className="flex items-center border-b-2 px-6 py-2 h-24 w-full" style={{ backgroundColor: '#4A90E2', position: 'sticky', top: 0, zIndex: 1000 }}>
-            <h1 className="font-bold" style={{ fontFamily: 'Newsreader, serif', color: 'white', fontSize: '37px' }}>SmileSeek</h1>
+        <Popover className="flex items-center border-b-2 px-6 py-2 h-24 w-full" style={{ backgroundColor: '#FFFF', position: 'sticky', top: 0, zIndex: 1000 }}>
+            <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}> {/* Ajustează gap-ul după nevoie */}
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Image src="/logo2-removebg-preview.png" alt="Logo" width={90} height={90} />
+                </div>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Image src="/smileSeek (2).png" alt="Smile" width={170} height={170} />
+                </div>
+            </div>
+
+
 
             <div className="grow">
                 <div className="hidden sm:flex items-center justify-end gap-2 md:gap-8 pr-5">
                     <Button variant="contained" sx={{ boxShadow: 'none', '&:hover': { boxShadow: 'none' }, color: 'black', border: 1, borderColor: 'black' }}>
                         <Link href="/home" style={{ color: 'black', textDecoration: 'none' }}>Exploreaza cabinetele</Link>
                     </Button>
-                    <Button variant="contained" sx={{ boxShadow: 'none', '&:hover': { boxShadow: 'none' }, color: 'white', border: 1, borderColor: 'black' }}>
+                    <Button variant="contained" sx={{ boxShadow: 'none', '&:hover': { boxShadow: 'none' }, color: 'black', border: 1, borderColor: 'black' }}>
                         <Link href="/blog">Alaturati-va ca profesionist</Link>
+                        {/* <MyBubblyLink to="/">Alaturati-va ca profesionist</MyBubblyLink> */}
+                        {/* <MyBubblyLink to="/test" text="a" /> */}
                     </Button>
                 </div>
 
