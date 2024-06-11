123553809c4f9e8c35a9cb1b2aee8490ad215341
Lazea Cristian <cristian.lazea01@e-uvt.ro>
2024-06-11 16:49:21 +0300

    Working on deployment

--- Shared/GlobalApi.tsx
+++ Shared/GlobalApi.tsx
@@ -1,6 +1,6 @@
 const { default: axios } = require("axios");
 
-const getGooglePlace = (category, radius, lat, lng) => axios.get('/api/google-place?' +
+const getGooglePlace = (category: string, radius: string, lat: string, lng: string) => axios.get('/api/google-place?' +
     'category=' + category + '&radius=' + radius + '&lat=' + lat + '&lng=' + lng)
 
 
