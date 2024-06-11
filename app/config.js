6a37d4e3baada6042c288ca4b3ec4115ae478965
Lazea Cristian <cristian.lazea01@e-uvt.ro>
2024-06-12 00:00:46 +0300

    modified base url

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:5000/api/v1";
-export const BASE_URL = "https://backend-eight-taupe.vercel.app";
+export const BASE_URL = "https://backend-eight-taupe.vercel.app/api/v1";
 // export const token = localStorage.getItem("token");
 export let token;
 if (typeof window !== "undefined") {
