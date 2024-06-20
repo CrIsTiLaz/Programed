fe03c2812225940f69fa4d1fbbfbcd4c84fd1b4e
Lazea Cristian <cristian.lazea01@e-uvt.ro>
2024-06-20 22:02:41 +0300

    Ad env

--- app/utils/uploadCloudinary.js
+++ app/utils/uploadCloudinary.js
@@ -2,6 +2,7 @@ const upload_preset = process.env.NEXT_PUBLIC_UPLOAD_PRESET;
 const cloud_name = process.env.NEXT_PUBLIC_CLOUD_NAME;
 
 const uploadImageToCloudinary = async (file) => {
+  console.log("cloud_name", cloud_name);
   const uploadData = new FormData();
   uploadData.append("file", file);
   uploadData.append("upload_preset", upload_preset);
