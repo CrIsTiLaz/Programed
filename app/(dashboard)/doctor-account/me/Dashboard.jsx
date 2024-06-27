d0579b19229d52813d7c3c99f28c803b3220c08a
Lazea Cristian <cristian.lazea01@e-uvt.ro>
2024-06-27 19:17:47 +0300

    Fixed doctor image from doctor dashboard

--- app/(dashboard)/doctor-account/me/Dashboard.jsx
+++ app/(dashboard)/doctor-account/me/Dashboard.jsx
@@ -1,92 +1,135 @@
-import React, { useState } from 'react'
-import Loading from '@/app/loading'
-import Error from '../../../error/Error'
+import React, { useState } from "react";
+import Loading from "@/app/loading";
+import Error from "../../../error/Error";
 // import { useGetProfile } from '../../../hooks/useFetchData'
-import useGetProfile from "../../../hooks/useFetchData"
-import { BASE_URL } from '@/app/config'
-import Tabs from './Tabs'
-import Image from 'next/image';
-import ClinicsAbout from '@/app/(components)/clinics/ClinicsAbout'
-import Profile from './Profile'
-import Appointments from './Appointments'
-import DoctorsAbout from '@/app/(components)/doctors/DoctorsAbout'
+import useGetProfile from "../../../hooks/useFetchData";
+import { BASE_URL } from "@/app/config";
+import Tabs from "./Tabs";
+import Image from "next/image";
+import ClinicsAbout from "@/app/(components)/clinics/ClinicsAbout";
+import Profile from "./Profile";
+import Appointments from "./Appointments";
+import DoctorsAbout from "@/app/(components)/doctors/DoctorsAbout";
 
 function Dashboard() {
+  const { data, loading, error } = useGetProfile(
+    `${BASE_URL}/doctors/profile/me`
+  );
 
-    const { data, loading, error } = useGetProfile(`${BASE_URL}/doctors/profile/me`)
+  const [tab, setTab] = useState("overview");
+  return (
+    <section>
+      <div className="max-w-[1170px] px-5 mx-auto">
+        {loading && !error && <Loading />}
+        {error && !loading && <Error errMessage={"errrrrrrrrrrr"} />}
 
-    const [tab, setTab] = useState('overview')
-    return (
-        <section>
-            <div className='max-w-[1170px] px-5 mx-auto'>
-                {loading && !error && <Loading />}
-                {error && !loading && <Error errMessage={"errrrrrrrrrrr"} />}
+        {!loading && !error && (
+          <div className="grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]">
+            <Tabs tab={tab} setTab={setTab} />
+            <div className="lg:col-span-2">
+              <div className="mt-8">
+                {tab === "overview" && (
+                  <div>
+                    <div className="flex items-center gap-4 mb-10">
+                      <figure
+                        className="items-center justify-center block lg:hidden"
+                        style={{
+                          width: "60%",
+                          height: "150px",
+                          position: "relative",
+                        }}
+                      >
+                        {data?.photo ? (
+                          <Image
+                            width={0}
+                            height={0}
+                            sizes="100vw"
+                            className="w-full h-auto object-cover"
+                            src={data?.photo}
+                            alt=""
+                          />
+                        ) : null}
+                      </figure>
 
-                {
-                    !loading && !error && <div className='grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]'>
-                        <Tabs tab={tab} setTab={setTab} />
-                        <div className="lg:col-span-2">
+                      <figure
+                        className="items-center justify-center hidden lg:flex"
+                        style={{
+                          width: "20%",
+                          height: "250px",
+                          position: "relative",
+                        }}
+                      >
+                        {data?.photo ? (
+                          <Image
+                            width={0}
+                            height={0}
+                            sizes="100vw"
+                            className="w-full h-auto object-cover"
+                            src={data?.photo}
+                            alt=""
+                          />
+                        ) : null}
+                      </figure>
 
+                      <div>
+                        <h3 className="text-[22px] leading-9 font-bold text-headingColor">
+                          {data.name}
+                        </h3>
 
-                            <div className='mt-8'>
-                                {tab === 'overview' && (
-                                    <div>
-                                        <div className='flex items-center gap-4 mb-10'>
-                                            <figure className='max-w-[200px] max-h-[200px]'>
-                                                {/* <Image src={data?.photo} alt="" width={200} height={200} className='w-full ' /> */}
-                                                {data?.photo ? <Image src={data?.photo} alt="" width={200} height={200} /> : null}
-                                            </figure>
+                        <div className="flex item-center gap-[6px] my-3">
+                          <span className="flex items-center gap-[6px] text-headingColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold">
+                            <Image
+                              src="/clinics/Star.png"
+                              alt=""
+                              width={20}
+                              height={20}
+                            />
+                            {data.averageRating?.toFixed(1)}
+                          </span>
 
-                                            <div>
-                                                <h3 className='text-[22px] leading-9 font-bold text-headingColor'>
-                                                    {data.name}
-                                                </h3>
-
-                                                <div className='flex item-center gap-[6px] my-3'>
-                                                    <span className='flex items-center gap-[6px] text-headingColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold'>
-                                                        <Image src='/clinics/Star.png' alt='' width={20} height={20} />
-                                                        {data.averageRating?.toFixed(1)}
-                                                    </span>
-
-                                                    <span className='text-textColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold'>
-                                                        ({data.totalRating})
-                                                    </span>
-                                                </div>
-
-                                                <div>
-                                                    <div className='bg-[#CCF0F3] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold'>
-                                                        {data.specialization}
-                                                    </div>
-                                                    <div className='bg-[#fff9ea] text-yellowColor py-1 px-4 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold rounded mt-2'>
-                                                        Medic {data.medicalGrade}
-                                                    </div>
-                                                </div>
-
-                                                <p className='text__para font-[15px] lg:max-w-[390px] leading-6'>
-                                                    {data?.bio}
-                                                </p>
-                                            </div>
-
-                                        </div>
-                                        <DoctorsAbout
-                                            name={data.name}
-                                            about={data.about}
-                                            qualifications={data.qualifications}
-                                        />
-                                    </div>)}
-                                {tab === 'appointments' && <Appointments appointments={data.appointments} doctorId={data._id} />}
-                                {tab === 'settings' && <Profile doctorData={data} />}
-                            </div>
+                          <span className="text-textColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold">
+                            ({data.totalRating})
+                          </span>
                         </div>
 
+                        <div>
+                          <div className="bg-[#CCF0F3] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold">
+                            {data.specialization}
+                          </div>
+                          <div className="bg-[#fff9ea] text-yellowColor py-1 px-4 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold rounded mt-2">
+                            Medic {data.medicalGrade}
+                          </div>
+                        </div>
+
+                        <p className="text__para font-[15px] lg:max-w-[390px] leading-6">
+                          {data?.bio}
+                        </p>
+                      </div>
                     </div>
-                }
+                    <DoctorsAbout
+                      name={data.name}
+                      about={data.about}
+                      qualifications={data.qualifications}
+                    />
+                  </div>
+                )}
+                {tab === "appointments" && (
+                  <Appointments
+                    appointments={data.appointments}
+                    doctorId={data._id}
+                  />
+                )}
+                {tab === "settings" && <Profile doctorData={data} />}
+              </div>
             </div>
-        </section>
-    )
+          </div>
+        )}
+      </div>
+    </section>
+  );
 }
 //22
-export default Dashboard
+export default Dashboard;
 //credentials for medic
 // User name: Cristi Doctor
 // password: 1234
--- app/(dashboard)/doctor-account/me/Tabs.jsx
+++ app/(dashboard)/doctor-account/me/Tabs.jsx
@@ -1,90 +1,141 @@
-import React, { useContext, useRef, useState } from 'react';
-import { BiMenu } from 'react-icons/bi';
-import { authContext } from '../../../context/AuthContext';
-import { useRouter, usePathname } from 'next/navigation';
+import React, { useContext, useRef, useState } from "react";
+import { BiMenu } from "react-icons/bi";
+import { authContext } from "../../../context/AuthContext";
+import { useRouter, usePathname } from "next/navigation";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 function Tabs({ tab, setTab }) {
+  const { dispatch } = useContext(authContext);
+  const navigate = useRouter();
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const { dispatch } = useContext(authContext);
-    const navigate = useRouter();
-    const [menuOpen, setMenuOpen] = useState(false);
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+    navigate.push("/");
+  };
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-    const handleLogout = () => {
-        dispatch({ type: 'LOGOUT' });
-        navigate.push('/');
-    };
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
+  const handleLinkClick = (tabName) => {
+    setTab(tabName);
+    toggleMenu();
+  };
 
-    const handleLinkClick = (tabName) => {
-        setTab(tabName);
-        toggleMenu();
-    };
+  const navRef = useRef();
 
-    const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
-    const showNavbar = () => {
-        navRef.current.classList.toggle(
-            "responsive_nav"
-        );
-    };
+  return (
+    <div>
+      <header>
+        <nav
+          ref={navRef}
+          className={`lg:hidden  ${menuOpen ? "showNavbar" : ""}`}
+          style={{
+            position: "fixed",
+            zIndex: 100,
+          }}
+        >
+          <button
+            onClick={() => handleLinkClick("overview")}
+            className={`${
+              tab === "overview"
+                ? "bg-indigo-100 text-primaryColor"
+                : "bg-transparent text-headingColor"
+            } w-full btn mt-5 rounded-md`}
+          >
+            Prezentare generală
+          </button>
+          <button
+            onClick={() => handleLinkClick("appointments")}
+            className={`${
+              tab === "appointments"
+                ? "bg-indigo-100 text-primaryColor"
+                : "bg-transparent text-headingColor"
+            } w-full btn mt-0 rounded-md`}
+          >
+            Programări
+          </button>
+          <button
+            onClick={() => handleLinkClick("settings")}
+            className={`${
+              tab === "settings"
+                ? "bg-indigo-100 text-primaryColor"
+                : "bg-transparent text-headingColor"
+            } w-full btn mt-0 rounded-md`}
+          >
+            Profil
+          </button>
+          <div className="mt-[100px] w-full">
+            <button
+              onClick={() => {
+                handleLogout();
+                toggleMenu();
+              }}
+              className="btn w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white"
+            >
+              Deconectare
+            </button>
+          </div>
+          <button className="nav-btn nav-close-btn " onClick={showNavbar}>
+            <FaTimes style={{ color: "black" }} />
+          </button>
+        </nav>
+        <button className="nav-btn" onClick={showNavbar}>
+          <BiMenu style={{ color: "black" }} />
+        </button>
+      </header>
+      <div
+        className={`hidden lg:flex flex-col p-[30px] bg-white shadow-panelShadow items-center h-max rounded-md`}
+      >
+        {/* Desktop Navigation */}
+        <button
+          onClick={() => setTab("overview")}
+          className={`${
+            tab === "overview"
+              ? "bg-indigo-100 text-primaryColor"
+              : "bg-transparent text-headingColor"
+          } w-full btn mt-0 rounded-md`}
+        >
+          Prezentare generală
+        </button>
 
-    return (
-        <div>
-            <header >
-                <nav ref={navRef} className={`lg:hidden  ${menuOpen ? 'showNavbar' : ''}`}>
-                    <button onClick={() => handleLinkClick('overview')} className={`${tab === 'overview' ? 'bg-indigo-100 text-primaryColor' : "bg-transparent text-headingColor"} w-full btn mt-5 rounded-md`}>
-                        Prezentare generală
-                    </button>
-                    <button onClick={() => handleLinkClick('appointments')} className={`${tab === 'appointments' ? 'bg-indigo-100 text-primaryColor' : "bg-transparent text-headingColor"} w-full btn mt-0 rounded-md`}>
-                        Programări
-                    </button>
-                    <button onClick={() => handleLinkClick('settings')} className={`${tab === 'settings' ? 'bg-indigo-100 text-primaryColor' : "bg-transparent text-headingColor"} w-full btn mt-0 rounded-md`}>
-                        Profil
-                    </button>
-                    <div className='mt-[100px] w-full'>
-                        <button onClick={() => { handleLogout(); toggleMenu(); }} className='btn w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white'>
-                            Deconectare
-                        </button>
+        <button
+          onClick={() => setTab("appointments")}
+          className={`${
+            tab === "appointments"
+              ? "bg-indigo-100 text-primaryColor"
+              : "bg-transparent text-headingColor"
+          } w-full btn mt-0 rounded-md`}
+        >
+          Programări
+        </button>
 
-                    </div>
-                    <button
-                        className="nav-btn nav-close-btn "
-                        onClick={showNavbar}>
-                        <FaTimes style={{ color: 'black' }} />
-                    </button>
-                </nav>
-                <button
-                    className="nav-btn"
-                    onClick={showNavbar}>
-                    <BiMenu style={{ color: 'black' }} />
-                </button>
+        <button
+          onClick={() => setTab("settings")}
+          className={`${
+            tab === "settings"
+              ? "bg-indigo-100 text-primaryColor"
+              : "bg-transparent text-headingColor"
+          } w-full btn mt-0 rounded-md`}
+        >
+          Profil
+        </button>
 
-            </header>
-            <div className={`hidden lg:flex flex-col p-[30px] bg-white shadow-panelShadow items-center h-max rounded-md`}>
-                {/* Desktop Navigation */}
-                <button onClick={() => setTab('overview')} className={`${tab === 'overview' ? 'bg-indigo-100 text-primaryColor' : "bg-transparent text-headingColor"} w-full btn mt-0 rounded-md`}>
-                    Prezentare generală
-                </button>
-
-                <button onClick={() => setTab('appointments')} className={`${tab === 'appointments' ? 'bg-indigo-100 text-primaryColor' : "bg-transparent text-headingColor"} w-full btn mt-0 rounded-md`}>
-                    Programări
-                </button>
-
-                <button onClick={() => setTab('settings')} className={`${tab === 'settings' ? 'bg-indigo-100 text-primaryColor' : "bg-transparent text-headingColor"} w-full btn mt-0 rounded-md`}>
-                    Profil
-                </button>
-
-                <div className='mt-[100px] w-full'>
-                    <button onClick={handleLogout} className='btn w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white'>
-                        Deconectare
-                    </button>
-                </div>
-            </div>
+        <div className="mt-[100px] w-full">
+          <button
+            onClick={handleLogout}
+            className="btn w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white"
+          >
+            Deconectare
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Tabs;
