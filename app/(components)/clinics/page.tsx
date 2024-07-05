b5f0c8f29a3e6971eeb491344acbd3ac4af5d967
Lazea Cristian <cristian.lazea01@e-uvt.ro>
2024-07-05 22:48:10 +0300

    Added more metadata

--- app/(components)/clinics/page.tsx
+++ app/(components)/clinics/page.tsx
@@ -1,6 +1,25 @@
 import { services } from '@/Shared/services'
 import React from 'react'
 import Clinics from './Clinics'
+export const metadata = {
+    title: "Cabinete - Programed",
+    description: "Descoperă cele mai bune cabinete medicale și programează-te rapid și ușor.",
+    alternates: {
+        canonical: `https://progra-med.ro/clinics`
+    },
+    openGraph: {
+        title: 'Cabinete - Programed',
+        description: 'Descoperă cele mai bune cabinete medicale și programează-te rapid și ușor.',
+        url: 'https://progra-med.ro/clinics',
+        images: [
+            {
+                url: 'https://progra-med.ro/og/og2.jpg', // Absolute URL to the image
+                width: 600,
+                height: 600,
+            },
+        ],
+    },
+};
 
 function page() {
     return (
--- app/(components)/contact/page.tsx
+++ app/(components)/contact/page.tsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import Contact from './Contact'
 
+export const metadata = {
+    title: "Contact - Programed",
+    description: "Contactează-ne rapid și ușor pentru programări la cei mai buni medici.",
+    alternates: {
+        canonical: `https://progra-med.ro/contact`
+    },
+    openGraph: {
+        title: 'Contact - Programed',
+        description: 'Contactează-ne rapid și ușor pentru programări la cei mai buni medici.',
+        url: 'https://progra-med.ro/contact',
+        images: [
+            {
+                url: 'https://progra-med.ro/og/og2.jpg', // Absolute URL to the image
+                width: 600,
+                height: 600,
+            },
+        ],
+    },
+};
+
 function page() {
     return (
         <div>
