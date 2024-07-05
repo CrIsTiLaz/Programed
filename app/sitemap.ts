3c5b4bf7b0de93f2172cce1faf31a27cbf2450ad
Lazea Cristian <cristian.lazea01@e-uvt.ro>
2024-07-05 21:43:52 +0300

    Modified sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,17 @@
+import { getAllPages } from './utils/getAllPages';
+
 export default async function sitemap() {
-    const url = "https://progra-med.ro"
-    return [
-        {
-            url:url,
-            lastModified: new Date(),
-        }
-    ]
+    const baseUrl = "https://progra-med.ro";
+    const pages = await getAllPages();
+    const hardcodedDate = "2024-07-05T00:00:00.000Z"; // Data hardcodata de astăzi
+
+    const sitemap = pages.map((page) => {
+        return {
+            url: `${baseUrl}${page.slug}`,
+            lastModified: hardcodedDate
+        };
+    });
+
+    return sitemap;
 }
+
---
+++ app/utils/getAllPages.js
@@ -0,0 +1,12 @@
+// lib/pages.js
+export async function getAllPages() {
+  // Exemplu de date. Aici ar trebui să adaugi logica pentru a obține toate paginile site-ului tău,
+  // fie dintr-o bază de date, fie dintr-un API etc.
+  return [
+    { slug: "/" }, // Pagina principală
+    { slug: "/clinics" }, // Pagina principală
+    { slug: "/contact" }, // Pagina principală
+
+    // Adaugă toate paginile tale aici
+  ];
+}
