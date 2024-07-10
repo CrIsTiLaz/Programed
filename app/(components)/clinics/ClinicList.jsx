a18c9a9ef65fb9cf8d8e8d79b9f4584a6354e530
Lazea Cristian <cristian.lazea01@e-uvt.ro>
2024-07-10 14:01:45 +0300

    Modified text from search button

--- app/(components)/clinics/ClinicList.jsx
+++ app/(components)/clinics/ClinicList.jsx
@@ -1,47 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import { BASE_URL } from '@/app/config';
-import Loading from '@/app/loading';
-import Error from '@/app/error/Error';
-import ClinicCard from './ClinicCard';
+import React, { useEffect, useState } from "react";
+import { BASE_URL } from "@/app/config";
+import Loading from "@/app/loading";
+import Error from "@/app/error/Error";
+import ClinicCard from "./ClinicCard";
 
 function ClinicList({ query, currentPage, perPage, setTotalPages }) {
-    const [clinics, setClinics] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [clinics, setClinics] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            setError(null);
-            try {
-                const response = await fetch(`${BASE_URL}/clinics?query=${query}&page=${currentPage}&perPage=${perPage}`);
-                const data = await response.json();
-                if (!response.ok) {
-                    throw new Error(data.message || 'Something went wrong');
-                }
-                setClinics(data.data);
-                setTotalPages(data.totalPages);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(
+          `${BASE_URL}/clinics?query=${query}&page=${currentPage}&perPage=${perPage}`
+        );
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.message || "Something went wrong");
+        }
+        setClinics(data.data);
+        setTotalPages(data.totalPages);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-            } catch (error) {
-                setError(error.message);
-            } finally {
-                setLoading(false);
-            }
+    fetchData();
+  }, [query, currentPage, perPage, setTotalPages]);
+  if (loading) return <Loading />;
+  if (error) return <Error errMessage={"Erroare"} />;
 
-        };
-
-        fetchData();
-    }, [query, currentPage, perPage, setTotalPages]);
-    if (loading) return <Loading />;
-    if (error) return <Error errMessage={'erroareeeeeeeeee'} />;
-
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5 ">
-            {clinics.map(clinic => (
-                <ClinicCard key={clinic._id} clinic={clinic} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5 ">
+      {clinics.map((clinic) => (
+        <ClinicCard key={clinic._id} clinic={clinic} />
+      ))}
+    </div>
+  );
 }
 
 export default ClinicList;
--- app/(components)/clinics/Clinics.jsx
+++ app/(components)/clinics/Clinics.jsx
@@ -1,106 +1,117 @@
 "use client"; // top to the file
-import React, { useState, useEffect } from 'react';
-import { BiSearch } from 'react-icons/bi';
-import ClinicList from './ClinicList';
-import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
-import { motion } from 'framer-motion';
+import React, { useState, useEffect } from "react";
+import { BiSearch } from "react-icons/bi";
+import ClinicList from "./ClinicList";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import { motion } from "framer-motion";
 
 function Clinics() {
-    const [query, setQuery] = useState('');
-    const [debounceQuery, setDebounceQuery] = useState("");
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
-    const perPage = 3;
+  const [query, setQuery] = useState("");
+  const [debounceQuery, setDebounceQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const perPage = 3;
 
-    const handleSearch = () => {
-        const sanitizedQuery = query.trim();
-        if (/^[a-zA-Z0-9\s]+$/.test(sanitizedQuery)) {
-            setQuery(sanitizedQuery);
-        } else {
-            console.error('Query invalid!');
-        }
+  const handleSearch = () => {
+    const sanitizedQuery = query.trim();
+    if (/^[a-zA-Z0-9\s]+$/.test(sanitizedQuery)) {
+      setQuery(sanitizedQuery);
+    } else {
+      console.error("Query invalid!");
     }
+  };
 
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            setDebounceQuery(query)
-        }, 700)
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebounceQuery(query);
+    }, 700);
 
-        return () => clearTimeout(timeout)
-    }, [query])
+    return () => clearTimeout(timeout);
+  }, [query]);
 
-    const handlePageChange = (newPage) => {
-        setCurrentPage(newPage);
-    };
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
-    return (
-        <>
-            <section>
-                <div className='container text-center'>
-                    <h2 className='heading'>Gaseste un cabinet</h2>
+  return (
+    <>
+      <section>
+        <div className="container text-center">
+          <h2 className="heading">Gaseste un cabinet</h2>
 
-                    <div className='max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] flex items-center justify-between rounded-[50px] relative'>
-                        <input type="search"
-                            className='py-4 pl-12 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor'
-                            placeholder="Cauta Cabinet dupa nume sau specializare"
-                            value={query}
-                            onChange={e => setQuery(e.target.value)} />
-                        <button className='bg-[#0066ff58] py-[15px] px-[35px] text-white font-[600] rounded-r-[50px]' onClick={handleSearch}>
-                            <BiSearch size={30} />
-                        </button>
-                    </div>
+          <div className="max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] flex items-center justify-between rounded-[50px] relative">
+            <input
+              type="search"
+              className="py-4 pl-12 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor"
+              placeholder="Cauta dupa nume / specializare"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button
+              className="bg-[#0066ff58] py-[15px] px-[35px] text-white font-[600] rounded-r-[50px]"
+              onClick={handleSearch}
+            >
+              <BiSearch size={30} />
+            </button>
+          </div>
 
-                    <section>
-                        <div className="container">
-                            <ClinicList
-                                query={debounceQuery}
-                                currentPage={currentPage}
-                                perPage={perPage}
-                                setTotalPages={setTotalPages}
-                            />
-                            <div className="flex justify-center items-center mt-8">
-                                <div className="flex gap-2">
-                                    <button
-                                        disabled={currentPage === 1}
-                                        onClick={() => handlePageChange(currentPage - 1)}
-                                        className={currentPage === 1 ? 'opacity-60  text-white' : ''}
-                                    >
-                                        <motion.div whileHover={{ scale: 1.1 }}>
-
-                                            <AiOutlineArrowLeft className="inline-block mr-2" /> Înapoi
-                                        </motion.div>
-
-
-                                    </button>
-                                    {Array.from({ length: totalPages }, (_, i) => i + 1).map(page => (
-                                        <button
-                                            key={page}
-                                            onClick={() => handlePageChange(page)}
-                                            className={currentPage === page ? 'bg-gray-100 rounded px-4 py-2' : ''}
-                                        >
-                                            {page}
-                                        </button>
-                                    ))}
-                                    <button
-                                        disabled={currentPage === totalPages}
-                                        onClick={() => handlePageChange(currentPage + 1)}
-                                        className={currentPage === totalPages ? 'opacity-60  text-white' : ''}
-                                    >
-                                        <motion.div whileHover={{ scale: 1.1 }}>
-                                            Înainte
-
-                                            <AiOutlineArrowRight className="inline-block ml-2" />
-                                        </motion.div>
-
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
+          <section>
+            <div className="container">
+              <ClinicList
+                query={debounceQuery}
+                currentPage={currentPage}
+                perPage={perPage}
+                setTotalPages={setTotalPages}
+              />
+              <div className="flex justify-center items-center mt-8">
+                <div className="flex gap-2">
+                  <button
+                    disabled={currentPage === 1}
+                    onClick={() => handlePageChange(currentPage - 1)}
+                    className={
+                      currentPage === 1 ? "opacity-60  text-white" : ""
+                    }
+                  >
+                    <motion.div whileHover={{ scale: 1.1 }}>
+                      <AiOutlineArrowLeft className="inline-block mr-2" />{" "}
+                      Înapoi
+                    </motion.div>
+                  </button>
+                  {Array.from({ length: totalPages }, (_, i) => i + 1).map(
+                    (page) => (
+                      <button
+                        key={page}
+                        onClick={() => handlePageChange(page)}
+                        className={
+                          currentPage === page
+                            ? "bg-gray-100 rounded px-4 py-2"
+                            : ""
+                        }
+                      >
+                        {page}
+                      </button>
+                    )
+                  )}
+                  <button
+                    disabled={currentPage === totalPages}
+                    onClick={() => handlePageChange(currentPage + 1)}
+                    className={
+                      currentPage === totalPages ? "opacity-60  text-white" : ""
+                    }
+                  >
+                    <motion.div whileHover={{ scale: 1.1 }}>
+                      Înainte
+                      <AiOutlineArrowRight className="inline-block ml-2" />
+                    </motion.div>
+                  </button>
                 </div>
-            </section>
-        </>
-    )
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Clinics;
